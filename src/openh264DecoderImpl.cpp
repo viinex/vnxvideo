58a44a995007082a3bc4d1e75d922cff625568a5
German Zhyvotnikov <german.zhivotnikov@gmail.com>
2019-12-23 22:37:31 +0200

    Properly implemented Flush for openh264 video decoder, + fix for holding the reference to decoder who holds the memory, -- by a CRawSample which we use to store decoder output

--- src/openh264DecoderImpl.cpp
+++ src/openh264DecoderImpl.cpp
@@ -20,22 +20,25 @@ public:
         long res = WelsCreateDecoder(&decoder);
         if (res != 0)
             throw std::runtime_error("WelsCreateDecoder failed: " + std::to_string(res));
-        m_decoder.reset(decoder, WelsDestroyDecoder);
 
         SDecodingParam param;
         memset(&param, 0, sizeof param);
         param.sVideoProperty.eVideoBsType = VIDEO_BITSTREAM_AVC;
-        res = m_decoder->Initialize(&param);
-        if (res != 0)
+        res = decoder->Initialize(&param);
+        if (res != 0) {
+            WelsDestroyDecoder(decoder);
             throw std::runtime_error("ISVCDecoder::Initialize failed: " + std::to_string(res));
+        }
+        m_decoder.reset(decoder, [](ISVCDecoder* d){
+            d->Uninitialize();
+            WelsDestroyDecoder(d);
+        });
+
 
         auto logcb = openh264log;
         m_decoder->SetOption(DECODER_OPTION_TRACE_CALLBACK, &logcb);
         auto loglevel = WELS_LOG_WARNING;
         m_decoder->SetOption(DECODER_OPTION_TRACE_LEVEL, &loglevel);
-    }
-    virtual ~COpenH264Decoder() {
-        m_decoder->Uninitialize();
     }
     virtual void Subscribe(VnxVideo::TOnFormatCallback onFormat, VnxVideo::TOnFrameCallback onFrame) {
         m_onFormat = onFormat;
@@ -53,26 +56,23 @@ public:
         uint8_t* dst[4] = { 0,0,0,0 };
         int res = m_decoder->DecodeFrameNoDelay(data, dataSize, dst, &bufferInfo);
         if (res != 0) {
-            VNXVIDEO_LOG(VNXLOG_DEBUG, "openh264decoder") << "DecodeFrameNoDelay returned error " << res;
+            VNXVIDEO_LOG(VNXLOG_DEBUG, "openh264decoder") << "ISVCDecoder::DecodeFrameNoDelay returned error " << res;
         }
         else if (bufferInfo.iBufferStatus == 1) {
-            int width = bufferInfo.UsrData.sSystemBuffer.iWidth;
-            int height = bufferInfo.UsrData.sSystemBuffer.iHeight;
-            callOnFormat(bufferInfo.UsrData.sSystemBuffer.iWidth, bufferInfo.UsrData.sSystemBuffer.iHeight);
-            switch (bufferInfo.UsrData.sSystemBuffer.iFormat) {
-            case videoFormatI420: {
-                int strides[4] = { bufferInfo.UsrData.sSystemBuffer.iStride[0],
-                    bufferInfo.UsrData.sSystemBuffer.iStride[1],bufferInfo.UsrData.sSystemBuffer.iStride[1],
-                    0 };
-                CRawSample sample(EMF_I420, width, height, strides, dst, false);
-                m_onFrame(&sample, bufferInfo.uiOutYuvTimeStamp);
-                break;
-            }
-            default: VNXVIDEO_LOG(VNXLOG_WARNING, "openh264decoder") << "Unsupported format of decoded frame: " << bufferInfo.UsrData.sSystemBuffer.iFormat;
-            }
+            pushDecoded(bufferInfo, dst);
         }
     }
     virtual void Flush() {
+        SBufferInfo bufferInfo;
+        memset(&bufferInfo, 0, sizeof bufferInfo);
+        uint8_t* dst[4] = { 0,0,0,0 };
+        int res = m_decoder->FlushFrame(dst, &bufferInfo);
+        if (res != 0) {
+            VNXVIDEO_LOG(VNXLOG_DEBUG, "openh264decoder") << "ISVCDecoder::FlushFrame returned error " << res;
+        }
+        else if (bufferInfo.iBufferStatus == 1) {
+            pushDecoded(bufferInfo, dst);
+        }
     }
 private:
     void callOnFormat(int width, int height) {
@@ -81,6 +81,22 @@ private:
             m_height = height;
             m_csp = EMF_I420;
             m_onFormat(m_csp, m_width, m_height);
+        }
+    }
+    void pushDecoded(const SBufferInfo& bufferInfo, uint8_t** dst) {
+        int width = bufferInfo.UsrData.sSystemBuffer.iWidth;
+        int height = bufferInfo.UsrData.sSystemBuffer.iHeight;
+        callOnFormat(bufferInfo.UsrData.sSystemBuffer.iWidth, bufferInfo.UsrData.sSystemBuffer.iHeight);
+        switch (bufferInfo.UsrData.sSystemBuffer.iFormat) {
+        case videoFormatI420: {
+            int strides[4] = { bufferInfo.UsrData.sSystemBuffer.iStride[0],
+                bufferInfo.UsrData.sSystemBuffer.iStride[1],bufferInfo.UsrData.sSystemBuffer.iStride[1],
+                0 };
+            CRawSample sample(EMF_I420, width, height, strides, dst, m_decoder); // m_decoder actually holds the memory buffer
+            m_onFrame(&sample, bufferInfo.uiOutYuvTimeStamp);
+            break;
+        }
+        default: VNXVIDEO_LOG(VNXLOG_WARNING, "openh264decoder") << "Unsupported format of decoded frame: " << bufferInfo.UsrData.sSystemBuffer.iFormat;
         }
     }
 };
